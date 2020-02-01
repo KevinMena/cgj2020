6a2acfd69ed9e1c9d5cbd6ab36c9e6c48a34d0a0
Kevin <kevin_mena3@hotmail.com>
2020-02-01 01:15:14 -0400

    Assets

---
+++ cgj-2020/Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField] Item module;
+
+    [SerializeField] const int NUMBER_SLOTS = 6; 
+
+    [SerializeField] List<Item> inventoryList = new List<Item>();
+
+    public void AddItem(Item itemToAdd)
+    {   
+        if(inventoryList.Count < NUMBER_SLOTS) 
+        {
+            inventoryList.Add(itemToAdd);
+            return;
+        } 
+        else
+        {
+            Debug.Log("Inventory Full");
+            return;
+        }
+    }
+
+    public Item GetRequestedItem(int requestedItem) 
+    {
+        // Lambda to find the object with the requested name
+        Item itemFound = inventoryList.Find(item => item.itemCode == requestedItem);
+        if(itemFound.itemCode != -1) {
+            return itemFound;
+        }
+
+        return null;
+    }
+}
---
+++ cgj-2020/Assets/Scripts/Inventory/Item.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Items/New Item")]
+public class Item : ScriptableObject
+{
+    public int itemCode = -1;
+    public string itemName = "New item";
+
+    public string itemDescription = "New description";
+
+    public GameObject owner;
+
+    public Sprite itemImage;
+}
