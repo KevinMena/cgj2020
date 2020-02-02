679192de6f280f8b4050bcc0b574599009feca27
Kevin <kevin_mena3@hotmail.com>
2020-02-02 08:57:41 -0400

    Changes to sewers, and scene in general

---
+++ cgj-2020/Assets/Scripts/Teleport.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public Canvas fadeCanvas;
+
+    public Transform tpPoint;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        StartCoroutine(TpCharacter(col.transform));
+    }
+
+    private IEnumerator TpCharacter(Transform character)
+    {
+        fadeCanvas.GetComponent<Animator>().SetTrigger("FadeOut");
+        yield return new WaitForSeconds(0.5f);
+        character.position = tpPoint.position;
+    }
+}
